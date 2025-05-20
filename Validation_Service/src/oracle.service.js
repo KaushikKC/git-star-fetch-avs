09b62a022bdecd704b565c839fca60f605e153f3
Ramees AR <64429541+ar-ramees@users.noreply.github.com>
2025-05-21 00:55:57 +0530

    Update oracle.service.js
    proper fallback

--- Validation_Service/src/oracle.service.js
+++ Validation_Service/src/oracle.service.js
@@ -9,8 +9,13 @@ async function getPrice(pair) {
         res = result.data;
 
     } catch (err) {
-      result = await axios.get(`https://api.binance.us/api/v3/ticker/price?symbol=${pair}`);
-      res = result.data;
+      try {
+        const result2 = await axios.get(`https://api.binance.us/api/v3/ticker/price?symbol=${pair}`);
+        res = result2.data;
+        
+      } catch (fallbackErr) {
+        console.error('Failed to get price: ', fallbackErr)
+      }
     }
     return res;
   }
